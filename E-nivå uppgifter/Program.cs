57d5b88177126cbf6773eb27c6948d33b40aa568
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:02:58 +0100

    Add project files.

---
+++ E-nivå uppgifter/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string[] namn = new string[5];
+Console.WriteLine("Skriv in 5 namn");
+
+namn[0] = "";
+namn[1] = "";
+namn[2] = "";
+namn[3] = "";
+namn[4] = "";
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("namn:");
+    namn[i] = Console.ReadLine();
+
+}
+Console.WriteLine($"Du skrev in {namn[0]}, {namn[1]}, {namn[2]}, {namn[3]}, {namn[4]}");
